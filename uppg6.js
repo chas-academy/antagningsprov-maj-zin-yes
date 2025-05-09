7d5a0770ae4ef413f426aedb51fc129c73ce6f30
zin-yes <juztz1n@gmail.com>
2025-05-09 15:03:57 +0200

    Implement uppg6

--- uppg6.js
+++ uppg6.js
@@ -1,11 +1,11 @@
+function uppg6() {
+  // Skapa en array med namn som du döper till names
+  const names = ["Hasse", "Hasselnöt", "Hasselbakspotatis"];
 
-
-function uppg6(){
-
-    // Skapa en array med namn som du döper till names
-    // Loopa igenom arrayen och skriv ut alla namn i konsolen
-
- 
+  // Loopa igenom arrayen och skriv ut alla namn i konsolen
+  names.forEach((name) => {
+    console.log(name);
+  });
 }
 
 module.exports = { uppg6 };
