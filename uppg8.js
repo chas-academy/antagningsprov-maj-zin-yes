3f2f7029ca80123e6f57a7476edb1475503748c6
zin-yes <juztz1n@gmail.com>
2025-05-09 15:04:57 +0200

    Implement uppg8

--- uppg8.js
+++ uppg8.js
@@ -1,17 +1,35 @@
+function uppg8() {
+  // skapa en array med fem person obkjet
+  // varje person objekt ska ha två properties, name och age
+  /**
+   * @typedef {Object} Person
+   * @property {string} name - Name of the person
+   * @property {number} age - Age of the person
+   */
+  const people = [
+    { name: "Hasse", age: 25 },
+    { name: "Hasselnöt", age: 35 },
+    { name: "Hasselbakspotatis", age: 40 },
+    { name: "Hasselblomma", age: 28 },
+    { name: "Hasselmus", age: 32 },
+  ];
 
+  // skapa en fuktion som tar in en array som argument
+  /**
+   * @param {Person[]} arr - An array of person objects
+   */
+  const func = (arr) => {
+    // skapa ett konditionsvillkor som gör att det bara är namnet på personerna som är
+    //  över 30 år som skrivs ut i konsolen
+    arr.forEach((person) => {
+      if (person.age > 30) {
+        console.log(person.name);
+      }
+    });
+  };
 
-function uppg8(){
-
-// skapa en array med fem person obkjet
-// varje person objekt ska ha två properties, name och age
-
-// skapa en fuktion som tar in en array som argument
-// skapa ett konditionsvillkor som gör att det bara är namnet på personerna som är
-//  över 30 år som skrivs ut i konsolen
-
-// anropa funktionen och skicka med arrayen som argument
-
-
+  // anropa funktionen och skicka med arrayen som argument
+  func(people);
 }
 
 module.exports = { uppg8 };
