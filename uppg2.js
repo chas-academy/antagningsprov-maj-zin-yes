82410f5016295628530d4e53c711c4b5b4314b90
zin-yes <juztz1n@gmail.com>
2025-05-09 14:59:06 +0200

    Implement uppg2

--- uppg2.js
+++ uppg2.js
@@ -1,7 +1,7 @@
 function uppg2() {
-  // Deklarera en variable med ditt namn och skriv ut det i konsolen
-  const name = "Hazhir";
-  consoel.log(name);
+  // deklarera en variable med ditt namn och skriv ut det i konsolen
+  const name = "Hasse";
+  console.log(name);
 }
 
 module.exports = { uppg2 };
