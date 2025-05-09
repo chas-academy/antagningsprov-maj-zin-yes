fa67e43701be2a16081aa5ca1bbe0d0825e604c9
zin-yes <juztz1n@gmail.com>
2025-05-09 15:02:20 +0200

    Implement uppg4

--- uppg4.js
+++ uppg4.js
@@ -1,12 +1,14 @@
-
 function uppg4() {
-  
-    // deklarera ett objekt med namnet person
-    // objektet ska ha två properties, name och age
+  // deklarera ett objekt med namnet person
+  // objektet ska ha två properties, name och age
+  const person = {
+    name: "Hasse",
     // tilldela age 25
-    // skriv ut med dotnotation åldern i konsolen
+    age: 25,
+  };
 
-
+  // skriv ut med dotnotation åldern i konsolen
+  console.log(person.age);
 }
 
 module.exports = { uppg4 };
