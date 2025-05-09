e70c4b6d82f832138841d40afc8118dde70fa4d0
zin-yes <juztz1n@gmail.com>
2025-05-09 15:04:22 +0200

    Implement uppg7

--- uppg7.js
+++ uppg7.js
@@ -1,12 +1,10 @@
-
-
 function uppg7() {
-   
-    // skapa en funktion som tar in två tal som argument
-    // funktionen ska returnera summan av talen
-    // kalla på funktionen i en return statement och ge den parametrarna 5 och 10
+  // skapa en funktion som tar in två tal som argument
+  // funktionen ska returnera summan av talen
+  const sum = (a, b) => a + b;
 
- 
+  // kalla på funktionen i en return statement och ge den parametrarna 5 och 10
+  return sum(5, 10);
 }
 
 module.exports = { uppg7 };
