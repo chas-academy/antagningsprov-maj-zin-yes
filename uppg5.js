2b1d79f1524a197720bbe26a432f68d3d8a9476b
zin-yes <juztz1n@gmail.com>
2025-05-09 15:02:50 +0200

    Implement uppg5

--- uppg5.js
+++ uppg5.js
@@ -1,11 +1,15 @@
-
 function uppg5() {
-    // skriv en if-sats som jämför två tal
+  const a = Math.floor(Math.random() * 100);
+  const b = Math.floor(Math.random() * 100);
+
+  // skriv en if-sats som jämför två tal
+  if (a > b) {
     // om det första talet är större än det andra, skriv ut "Det första talet är större än det andra"
+    console.log("Det första talet är större än det andra");
+  } else {
     // annars skriv ut "Det andra talet är större än det första"
-
-
-
+    console.log("Det andra talet är större än det första");
+  }
 }
 
 module.exports = { uppg5 };
