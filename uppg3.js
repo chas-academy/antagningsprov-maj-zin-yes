98935553bd899195b930339b332415af5cc3423b
zin-yes <juztz1n@gmail.com>
2025-05-09 15:01:44 +0200

    Implement uppg3

--- uppg3.js
+++ uppg3.js
@@ -1,15 +1,13 @@
-
-
 function uppg3() {
+  // Skriv ut summan av två tal
+  // deklarera två variabler, a och b
+  // tilldela värdena 5 och 10 till variablerna
+  const a = 5;
+  const b = 10;
 
-    // Skriv ut summan av två tal
-    // deklarera två variabler, a och b
-    // tilldela värdena 5 och 10 till variablerna
-    // skriv ut summan av a och b i konsolen
-
-
-
+  // skriv ut summan av a och b i konsolen
+  const sum = a + b;
+  console.log(sum);
 }
-
 
 module.exports = { uppg3 };
