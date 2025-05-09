d820a50f10a714cf94f781d7e443e5f610b3483d
zin-yes <juztz1n@gmail.com>
2025-05-09 15:06:15 +0200

    Implement uppg9

--- uppg9.js
+++ uppg9.js
@@ -1,14 +1,28 @@
+function uppg9() {
+  // skapa en funktion som heter sort och tar in en array av siffror som argument
+  /**
+   * @param {number[]} arr - An array of numbers
+   */
+  const sort = (arr) => {
+    arr.forEach((num) => {
+      // i funktionen du loopa igenom arrayen och kolla med en if-sats om talet är jämt eller udda
+      // resultatet ska loggas i formatet: talet och sen ordet "jämt" eller "udda"
+      // i if-sattsen skriv ut i konsolen resultatet
+      if (num % 2 === 0) {
+        console.log(num, "jämt");
+      } else {
+        console.log(num, "udda");
+      }
+    });
+  };
 
+  const arr = [];
+  for (let i = 0; i < 10; i++) {
+    arr.push(Math.floor(Math.random() * 10));
+  }
 
-function uppg9 (){
-    // skapa en funktion som heter sort och tar in en array av siffror som argument
-    // i funktionen du loopa igenom arrayen och kolla med en if-sats om  talet är jämt eller udda
-    // i if-sattsen skriv ut i konsolen resultatet
-    // resultatet ska loggas i formatet: talet och sen ordet "jämt" eller "udda"
-
-    // anropa funktionen och skicka med en array som argument
-
-  
+  // anropa funktionen och skicka med en array som argument
+  sort(arr);
 }
 
 module.exports = { uppg9 };
